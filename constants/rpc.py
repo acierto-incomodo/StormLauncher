129c423ef3e7c2db915599b95177ad6677477b6f
acierto-incomodo <97026099+acierto-incomodo@users.noreply.github.com>
2023-06-04 11:47:40 +0200

    Update rpc.py

--- constants/rpc.py
+++ constants/rpc.py
@@ -1,10 +1,10 @@
 # Discord Rich Presence Settings
 from __future__ import annotations
 
-ENABLED = False
-CLIENT_ID = "673338815301287966"
-LARGE_IMAGE = "pymymc_logo"
+ENABLED = True
+CLIENT_ID = "1114577470893609053"
+LARGE_IMAGE = "stormlauncher_logo"
 CONFIG_IMAGE = "config"
 ROOT_IMAGE = "main"
-VANILLA_IMAGE = "vanilla"
+VANILLA_IMAGE = "minecraft"
 MODDED_IMAGE = "modded"
